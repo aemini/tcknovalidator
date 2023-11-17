df737db7981d96573679874f5c5dd93be015e49a
Arya Emini <arya@aryaemini.com>
2023-11-18 02:39:05 +0300

    java 17 update added models

---
+++ src/main/java/com/aryaemini/nvi/interfaces/Person.java
@@ -0,0 +1,13 @@
+package com.aryaemini.nvi.interfaces;
+
+public interface Person {
+
+	String getIdentityNumber();
+
+	String getFirstName();
+
+	String getLastName();
+
+	Integer getBirthYear();
+
+}
