12c3988d96473ef2794a493bde710c61f7e8010c
Arya Emini <arya@aryaemini.com>
2024-10-17 10:31:24 +0300

    NullPointerException yakaladigim yerde NullPointerException yaratmisim

--- src/main/java/com/aryaemini/nvi/model/Citizen.java
+++ src/main/java/com/aryaemini/nvi/model/Citizen.java
@@ -1,27 +1,32 @@
 package com.aryaemini.nvi.model;
 
 import com.aryaemini.nvi.interfaces.Person;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Objects;
 
-@Builder
 @Getter
+@SuperBuilder(toBuilder=true)
 @ToString
 public class Citizen implements Person {
 
+	protected static final Calendar calendar = Calendar.getInstance();
 	private String identityNumber;
 	private String firstName;
 	private String lastName;
 	private Date birthDate;
+	private Integer birthYear;
 
 	public Integer getBirthYear() {
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTime(birthDate);
-		return calendar.get(Calendar.YEAR);
+		if (Objects.nonNull(birthDate)) {
+			calendar.setTime(birthDate);
+			birthYear = calendar.get(Calendar.YEAR);
+		}
+		return birthYear;
 	}
 
 }
